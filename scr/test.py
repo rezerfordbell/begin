95d6533e0b2f8f833a369748e8ea5eaf566f0406
Alex <AKaraliou@ibagroup.eu>
2021-02-03 20:18:27 +0300

    let's add some tests again and again and again and again and again and again and again and again and again and again and again and again and again

--- scr/test.py
+++ scr/test.py
@@ -1,9 +1,14 @@
 import unittest
+import requests
+
+BASE_API_URL = "https://randomuser.me/api/?format=json"
+
 
 class TestMethods(unittest.TestCase):
 
-  def test_upper(self):
-      self.assertEqual('foo'.upper(), 'FOO')
+    def test_api_works(self):
+        result = requests.get(BASE_API_URL)
+        self.assertEqual(result.status_code, 200)
 
 
 if __name__ == '__main__':
